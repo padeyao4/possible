b38b4cc4571fe70d79e3e8294eb44e0eeca235cc
Jian Guo <guojian_k@qq.com>
2022-04-19 01:17:08 +0800

    整改less写法

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Body from '@/components/Body';
 import Header from '@/components/Header';
 import { useModel } from 'umi';
 import { Project } from '../core/model';
-import styles from './index.less';
+import './index.less';
 
 export default function () {
   const { projects, setProjects, setIndex } = useModel('project');
@@ -19,23 +19,23 @@ export default function () {
   };
 
   return (
-    <section className={styles.main}>
-      <div className={styles.side}>
+    <section className="main">
+      <div className="side">
         <div>
           <div>
-            <div className={styles.myButton} onClick={() => setIndex(-2)}>
+            <div className="button" onClick={() => setIndex(-2)}>
               我的一天
             </div>
-            <div className={styles.myButton} onClick={() => setIndex(-1)}>
+            <div className="button" onClick={() => setIndex(-1)}>
               任务总览
             </div>
           </div>
-          <hr className={styles.myHr} />
-          <div className={styles.scrollPane}>
+          <hr />
+          <div className="scrollPane">
             {projects.map((project, index) => {
               return (
                 <div
-                  className={styles.myButton}
+                  className="button"
                   key={index}
                   onClick={() => setIndex(index)}
                 >
@@ -44,18 +44,18 @@ export default function () {
               );
             })}
           </div>
-          <hr className={styles.myHr} />
+          <hr />
         </div>
         <div>
-          <div className={styles.myButton} onClick={handleProjectClick}>
+          <div className="button" onClick={handleProjectClick}>
             新建项目
           </div>
         </div>
       </div>
-      <div className={styles.content}>
+      <div className="content">
         <Header />
         <Body />
-        <div className={styles.footer}>
+        <div className="footer">
           <div onClick={today}>回到今天</div>
           <div onClick={yesterday}>左</div>
           <input type="range" max="100" min="1" defaultValue="50"></input>
