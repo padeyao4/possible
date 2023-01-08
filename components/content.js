780a46c1a5346e17774d91e27ddc71fe0a8b7590
Jian Guo <guojian_k@qq.com>
2023-01-08 15:21:14 +0800

    update

---
+++ components/content.js
@@ -0,0 +1,7 @@
+import styles from './Content.module.css'
+import Flow from './flow'
+export default function Content() {
+    return <div className={styles.main}>
+        <Flow></Flow>
+    </div>
+}
