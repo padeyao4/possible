32dc4eca58ed66b1f96f8d0c8bac228b6c6fd8a3
padeyao4 <guojian_k@qq.com>
2023-07-24 23:50:48 +0800

    init vue

--- src/components/content.tsx
+++
@@ -1,8 +0,0 @@
-import Flow from "./flow";
-import styles from './content.module.css';
-
-export default function Content() {
-    return <div className={styles.main}>
-        <Flow></Flow>
-    </div>
-}
