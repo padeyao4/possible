4062e3f92efe8f92be760ab38ae6f555b6b38dc7
padeyao4 <guojian_k@qq.com>
2023-09-24 09:39:07 +0800

    update

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -2,7 +2,6 @@
 import { registerBehaviors } from '@renderer/g6'
 import { useGlobalStore } from '@renderer/store/global'
 import NewProjectButton from '@renderer/components/NewProjectButton.vue'
-import SideListItem from '@renderer/components/SideListItem.vue'
 import router from '@renderer/router'
 import { RouterView } from 'vue-router'
 
@@ -17,6 +16,14 @@ const handleTodayClick = () => {
     replace: true
   })
 }
+
+const handleItemClick = (id: string) => {
+  router.push({
+    name: 'summery',
+    replace: true
+  })
+  store.$patch({ active: id })
+}
 </script>
 
 <template>
@@ -30,12 +37,22 @@ const handleTodayClick = () => {
           <div style="margin-left: 4px; padding-bottom: 1px">我的一天</div>
         </div>
         <hr />
-        <side-list-item />
+        <div class="list">
+          <div
+            v-for="item in store.projects"
+            :key="item.id"
+            class="list-item"
+            :class="{ active: item.id === store.active }"
+            @click="handleItemClick(item.id)"
+          >
+            {{ item.name }}
+          </div>
+        </div>
         <hr />
         <new-project-button />
       </div>
       <div class="content">
-        <router-view></router-view>
+        <router-view />
       </div>
     </div>
   </div>
@@ -68,6 +85,23 @@ const handleTodayClick = () => {
         user-select: none;
       }
 
+      .list {
+        height: calc(100vh - 82px);
+        overflow-y: auto;
+
+        .list-item {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          height: 40px;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          user-select: none;
+        }
+      }
+
       .active {
         background: var(--color-neptune);
       }
@@ -77,5 +111,27 @@ const handleTodayClick = () => {
       width: 100%;
     }
   }
+}
+
+/* 滚动条设置 */
+::-webkit-scrollbar {
+  width: 3px;
+}
+
+::-webkit-scrollbar-track {
+  width: 2px;
+  /* background-color: #1c1c1c; */
+  border-radius: 2em;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #585858;
+  background-clip: padding-box;
+  min-height: 28px;
+  border-radius: 2em;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  width: 6px;
 }
 </style>
