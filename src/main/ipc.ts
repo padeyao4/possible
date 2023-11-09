0fa52477e71ed29840a8ed4e10f2013da9175284
Jian Guo <guojian_k@qq.com>
2023-11-09 20:04:18 +0800

    添加自动更新功能

---
+++ src/main/ipc.ts
@@ -0,0 +1,75 @@
+import {autoUpdater, UpdateInfo} from 'electron-updater';
+import {dialog, ipcMain} from "electron";
+
+export function updater() {
+    // 触发检查更新(此方法用于被渲染线程调用，例如页面点击检查更新按钮来调用此方法)
+    ipcMain.on('check-for-update', () => {
+        console.info('触发检查更新');
+        autoUpdater.checkForUpdates().then(r => console.debug(r));
+    });
+
+// 设置自动下载为false(默认为true，检测到有更新就自动下载)
+    autoUpdater.autoDownload = false;
+// 检测下载错误
+    autoUpdater.on('error', (error) => {
+        console.error('更新异常', error);
+    });
+
+// 检测是否需要更新
+    autoUpdater.on('checking-for-update', () => {
+        console.info('正在检查更新……');
+    });
+// 检测到可以更新时
+    autoUpdater.on('update-available', (releaseInfo: UpdateInfo) => {
+        console.info('检测到新版本，确认是否下载');
+        const releaseNotes = releaseInfo.releaseNotes;
+        let releaseContent = '';
+        if (releaseNotes) {
+            if (typeof releaseNotes === 'string') {
+                releaseContent = <string>releaseNotes;
+            } else {
+                releaseNotes.forEach((releaseNote) => {
+                    releaseContent += `${releaseNote}\n`;
+                });
+            }
+        } else {
+            releaseContent = '暂无更新说明';
+        }
+        // 弹框确认是否下载更新（releaseContent是更新日志）
+        dialog
+            .showMessageBox({
+                type: 'info',
+                title: '应用有新的更新',
+                detail: releaseContent,
+                message: '发现新版本，是否现在更新？',
+                buttons: ['否', '是']
+            })
+            .then(({response}) => {
+                if (response === 1) {
+                    // 下载更新
+                    autoUpdater.downloadUpdate().then(r => console.debug(r));
+                }
+            });
+    });
+    // 检测到不需要更新时
+    autoUpdater.on('update-not-available', () => {
+        console.info('现在使用的就是最新版本，不用更新');
+    });
+    // 更新下载进度
+    autoUpdater.on('download-progress', (progress) => {
+        console.info('下载进度', progress);
+    });
+    // 当需要更新的内容下载完成后
+    autoUpdater.on('update-downloaded', () => {
+        console.info('下载完成，准备更新');
+        dialog
+            .showMessageBox({
+                title: '安装更新',
+                message: '更新下载完毕，应用将重启并进行安装'
+            })
+            .then(() => {
+                // 退出并安装应用
+                setImmediate(() => autoUpdater.quitAndInstall());
+            });
+    });
+}
---
+++ src/main/util.ts
@@ -0,0 +1,26 @@
+import {join} from "path";
+import icon from '../../resources/icon.png?asset'
+import {is} from "@electron-toolkit/utils";
+
+/**
+ * 获取icon
+ */
+export function getIcon() {
+    if (process.platform === 'linux') {
+        return {icon}
+    }
+    if (process.platform == 'win32') {
+        return {icon: join(__dirname, '../../resources/icon.ico')}
+    }
+    return {icon}
+}
+
+/**
+ * 获取possible home目录地址
+ */
+export function getPossibleHome() {
+    const USER_HOME = process.env.HOME || process.env.USERPROFILE || '~/'
+    const dev = is.dev && process.env['ELECTRON_RENDERER_URL']
+    // 检查possible_home变量是否存在
+    return dev ? join(USER_HOME, '.possible-dev') : (process.env.POSSIBLE_HOME || join(USER_HOME, '.possible'))
+}
