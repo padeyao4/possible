7b9c59b19fd6247bd9e84898faf8beb5b89c65e2
Jian Guo <guojian_k@qq.com>
2023-11-27 17:51:52 +0800

    添加任务图标更新

--- src/renderer/src/views/TodayView/component/CreateTaskInput.vue
+++ src/renderer/src/views/TodayView/component/CreateTaskInput.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-import {ListAdd, Plus} from "@icon-park/vue-next";
+import {ListAdd, Plus, Round} from "@icon-park/vue-next";
 </script>
 
 <template>
   <div class="create-task-input">
     <div class="footer-input">
-      <plus theme="outline" size="24" fill="#333" :strokeWidth="2" class="icon"/>
-      <input class="input"/>
       <list-add theme="outline" size="24" fill="#333" :strokeWidth="2" class="icon"/>
+      <input class="input" placeholder="添加任务"/>
+      <plus theme="outline" size="24" fill="#333" :strokeWidth="2" class="icon plus"/>
+      <Round theme="outline" size="24" fill="#333" :strokeWidth="2" strokeLinecap="butt" class="icon round"/>
     </div>
   </div>
 </template>
@@ -15,10 +16,11 @@ import {ListAdd, Plus} from "@icon-park/vue-next";
 <style scoped>
 .create-task-input {
   height: 100%;
-  padding: 8px 24px 8px 24px;
+  padding: 8px 24px 16px 24px;
 
   .footer-input {
     display: flex;
+    flex-direction: row-reverse;
     align-items: center;
     height: 100%;
     border-radius: 4px;
@@ -31,6 +33,20 @@ import {ListAdd, Plus} from "@icon-park/vue-next";
       margin: 8px;
     }
 
+    .plus {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 8px;
+    }
+
+    .round {
+      display: none;
+      justify-content: center;
+      align-items: center;
+      margin: 8px;
+    }
+
     .input {
       display: flex;
       align-items: center;
@@ -38,12 +54,16 @@ import {ListAdd, Plus} from "@icon-park/vue-next";
       height: 100%;
       border: 0;
       outline-style: none;
+      padding: 0 8px;
       background: rgba(255, 255, 255, 0.7);
     }
 
-    & * > {
+    .input:focus ~ .plus {
+      display: none;
+    }
+
+    .input:focus ~ .round {
       display: flex;
-      align-items: center;
     }
   }
 }
