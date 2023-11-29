77acd141533852a2bab8b7f25f9c90f95fe5a899
Jian Guo <guojian_k@qq.com>
2023-11-29 14:21:40 +0800

    添加确认框

--- src/renderer/src/views/LayoutView/component/ListContextMenu.vue
+++ src/renderer/src/views/LayoutView/component/ListContextMenu.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import {watch} from "vue";
+import {ref, watch} from "vue";
 import {useProjectStore} from "@renderer/store/project";
 
 const props = defineProps<{
@@ -26,12 +26,20 @@ watch(() => props.visible, () => {
   }
 })
 
+const project = projectStore.get(props.projectId)
+
+const deleteDialogVisible = ref(false)
+
+function handleProjectDelete() {
+  projectStore.delete(props.projectId)
+}
+
 function handleList(e: any) {
   const opt = (e.target?.id ?? 'default') as string
   switch (opt) {
     case 'delete':
       console.log(props.projectId);
-      projectStore.delete(props.projectId)
+      deleteDialogVisible.value = true
       break;
     default:
       console.warn('do nothing')
@@ -45,6 +53,19 @@ function handleList(e: any) {
   <div class="list-context-menu"
        :style="{'display': visible?'flex':'none','left': x+'px','top': y+'px'}"
   >
+    <Teleport to="body">
+      <el-dialog v-model="deleteDialogVisible" title="警告" width="30%" align-center>
+              <span
+              >确定删除 <i style="font-size: large">{{ project.name ?? '' }} </i> 计划吗</span
+              >
+        <template #footer>
+                <span class="dialog-footer">
+                  <el-button type="primary" @click="deleteDialogVisible = false">取消</el-button>
+                  <el-button @click="handleProjectDelete"> 确定 </el-button>
+                </span>
+        </template>
+      </el-dialog>
+    </Teleport>
     <div class="content">
       <ul @click="(e)=>handleList(e)">
         <li id="delete">删除</li>
