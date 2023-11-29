b76d8e7be5ad2e78e1a447de4614a10ef93adc87
Jian Guo <guojian_k@qq.com>
2023-11-30 02:22:11 +0800

    修复部分添加任务错误

--- src/renderer/src/views/TodayView/component/CreateTaskInput.vue
+++ src/renderer/src/views/TodayView/component/CreateTaskInput.vue
@@ -1,15 +1,76 @@
 <script setup lang="ts">
 import {ListAdd, Plus, Round} from "@icon-park/vue-next";
+import {useProjectStore} from "@renderer/store/project";
+import {computed, ref, watch} from "vue";
+import {Possible} from "@renderer/model";
+import IProject = Possible.IProject;
+
+const projectStore = useProjectStore()
+
+const projects = computed(() => {
+  return projectStore.list
+})
+
+const addonVisible = ref(false)
+
+function changeAddonVisible() {
+  addonVisible.value = false
+}
+
+watch(addonVisible, () => {
+  if (addonVisible.value) {
+    document.addEventListener('click', changeAddonVisible)
+  } else {
+    document.removeEventListener('click', changeAddonVisible)
+  }
+})
+
+const listItemHeight = 30;
+
+const selectValue = ref<IProject>(projectStore.list?.[0])
+watch(() => projectStore.list, () => {
+  const id = selectValue.value?.id
+  selectValue.value = projectStore.get(id as string)
+  if (!selectValue.value?.id) {
+    selectValue.value = projectStore.list?.[0]
+  }
+})
+
+function handleClickAddon(project: IProject) {
+  selectValue.value = project
+}
+
+const inputValue = ref('')
+
+function handleSubmit() {
+  console.log(inputValue.value, selectValue.value?.name)
+  if (selectValue.value?.id) {
+    const node = new Possible.Node(inputValue.value, selectValue.value.id)
+    selectValue.value.data.nodes.push(node)
+  } else {
+    const projectId = projectStore.createByName('默认')
+    const node = new Possible.Node(inputValue.value, projectId)
+    projectStore.get(projectId).data.nodes.push(node)
+  }
+  inputValue.value = ''
+}
+
 </script>
 
 <template>
   <div class="create-task-input">
     <div class="footer-input">
-      <div class="addon">
+      <div class="addon" @click.stop="addonVisible=!addonVisible">
+        <div class="list" v-show="addonVisible"
+             :style="{'top':`calc(${projects.length * listItemHeight}px * -1 - 4px)`,'height':`${projects.length * listItemHeight}px`}">
+          <div v-for="item in projects" :style="{'height':listItemHeight}" class="item"
+               @click="()=>{handleClickAddon(item)}">{{ item.name }}
+          </div>
+        </div>
         <list-add theme="outline" size="24" fill="#333" :strokeWidth="2" class="icon"/>
-        <div class="text">默认</div>
+        <div class="text">{{ selectValue?.name ?? '默认' }}</div>
       </div>
-      <input class="input" placeholder="添加任务"/>
+      <input class="input" v-model="inputValue" placeholder="添加任务" @keydown.enter="handleSubmit"/>
       <plus theme="outline" size="24" fill="#333" :strokeWidth="2" class="icon plus"/>
       <Round theme="outline" size="24" fill="#333" :strokeWidth="2" strokeLinecap="butt" class="icon round"/>
     </div>
@@ -30,15 +91,60 @@ import {ListAdd, Plus, Round} from "@icon-park/vue-next";
     background: rgba(255, 255, 255, 0.7);
 
     .addon {
+      position: relative;
       display: flex;
-      .text{
+      user-select: none;
+      max-width: 160px !important;
+
+      .text {
         font-weight: 1;
         font-size: 14px;
         display: flex;
+        justify-content: start;
+        align-items: center;
+        margin: 0 8px 0 0;
+        width: 100%;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        text-align: center;
+        word-break: break-all;
+        white-space: nowrap;
+      }
+
+      .list {
+        display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
-        margin: 4px;
-        width: max-content;
+        position: absolute;
+        width: 100px;
+        left: calc(100% - 100px);
+        background: var(--color-background);
+        border-radius: 4px;
+        box-shadow: rgba(0, 0, 0, 0.24) 0 3px 8px;
+
+        .item {
+          display: flex;
+          justify-content: start;
+          align-items: center;
+          padding: 4px;
+          width: 100%;
+          font-size: 14px;
+          font-weight: 1;
+          text-overflow: ellipsis;
+          overflow: hidden;
+          text-align: center;
+          word-break: break-all;
+          white-space: nowrap;
+
+          &:hover {
+            background: #82bbb550;
+          }
+        }
+      }
+
+      &:hover {
+        background: #82bbb550;
       }
     }
 
