f862be274820411c093741120b46072d4aae62bc
Jian Guo <guojian_k@qq.com>
2024-03-13 16:10:28 +0800

    add time axios

--- src/components/TimerHeader.vue
+++ src/components/TimerHeader.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject, onBeforeUnmount, onMounted, ref, shallowRef, watchEffect } from 'vue'
+import { inject, onBeforeUnmount, onMounted, ref, shallowRef, watch, watchEffect } from 'vue'
 import { Canvas, Group, Text } from '@antv/g'
 import { Renderer } from '@antv/g-canvas'
 
@@ -8,13 +8,50 @@ const container = ref()
 const canvas = shallowRef()
 const group = shallowRef()
 
-function changePosition() {
+watch(group, () => {
+  updateInfo()
+})
+
+function updateInfo() {
+  if (!graph.value) return
   const { x } = graph.value.getCanvasByViewport({ x: 0, y: 0 })
-  group.value.setPosition(-x, 0)
+  group.value.setPosition(-x % 120, 0)
+  const children = group.value.getChildren()
+  const offset = x / 120
+  const delta = offset >= 0 ? Math.floor(offset) : Math.ceil(offset)
+  for (let i = 0; i < children.length; i++) {
+    const index = children[i].id
+    children[i].style.text = (index + delta).toString()
+  }
 }
 
 function resize() {
   canvas.value.resize(container.value.clientWidth, container.value.clientHeight)
+  initTexts()
+}
+
+function initTexts() {
+  group.value.removeChildren()
+  const childrenSize = Math.ceil(container.value.clientWidth / 120) + 1
+  for (let i = -1; i < childrenSize; i++) {
+    const text = new Text({
+      id: i,
+      style: {
+        x: i * 120,
+        y: 20,
+        text: '',
+        fontSize: 14,
+        fill: '#000000',
+        textBaseline: 'middle',
+        textAlign: 'center',
+        wordWrapWidth: 100,
+        wordWrap: true,
+        lineWidth: 5
+      }
+    })
+    group.value.appendChild(text)
+  }
+  updateInfo()
 }
 
 onMounted(() => {
@@ -26,36 +63,19 @@ onMounted(() => {
     })
 
     group.value = new Group()
-
-    const text = new Text({
-      style: {
-        x: 10,
-        y: 20,
-        text: '这是测试文本This is text',
-        fontSize: 14,
-        fill: '#000000',
-        textBaseline: 'middle',
-        wordWrapWidth: 100,
-        wordWrap: true,
-        lineWidth: 5
-      }
-    })
-
-    group.value.appendChild(text)
-
     canvas.value.appendChild(group.value)
+    initTexts()
 
     watchEffect(() => {
-      graph.value?.on('viewportchange', changePosition)
+      graph.value?.on('viewportchange', updateInfo)
     })
 
     window.addEventListener('resize', resize)
   }
 )
 
-
 onBeforeUnmount(() => {
-  graph.value.off('viewportchange', changePosition)
+  graph.value.off('viewportchange', updateInfo)
   window.removeEventListener('resize', resize)
 })
 </script>
