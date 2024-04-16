dc2f6bca7d5373049d8618e46f807d6947a1bcce
Jian Guo <guojian_k@qq.com>
2024-04-16 23:44:08 +0800

    解决边框鼠标指针闪烁

--- src/components/WindowTitlebar.vue
+++ src/components/WindowTitlebar.vue
@@ -37,6 +37,16 @@ const showTitlebar = import.meta.env?.VITE_TITLEBAR === 'true'
     v-if="showTitlebar"
     :class="['titlebar',{'cancel-radius':settings.isMaximize}]">
     <div class="drag-area" data-tauri-drag-region></div>
+    <div class="mouse-style">
+      <div class="top"></div>
+      <div class="bottom"></div>
+      <div class="left"></div>
+      <div class="right"></div>
+      <div class="top-left"></div>
+      <div class="top-right"></div>
+      <div class="bottom-left"></div>
+      <div class="bottom-right"></div>
+    </div>
     <div class="titlebar-button system-icon-other" id="titlebar-minimize" @click="appWindow.minimize()">
       <Icon icon="clarity:window-min-line" width="20" height="20" class="system-icon" />
     </div>
@@ -63,6 +73,108 @@ const showTitlebar = import.meta.env?.VITE_TITLEBAR === 'true'
   top: 0;
   left: 0;
   right: 0;
+
+  .drag-area {
+    width: 100%;
+    height: 100%;
+    margin: 5px 0 0 5px;
+  }
+
+  .mouse-style {
+    position: fixed;
+
+    .top {
+      position: fixed;
+      height: 5px;
+      width: calc(100vw - 10px);
+      top: 0;
+      left: 5px;
+
+      &:hover {
+        cursor: n-resize;
+      }
+    }
+
+    .bottom {
+      position: fixed;
+      height: 5px;
+      width: calc(100vw - 10px);
+      bottom: 0;
+      left: 5px;
+
+      &:hover {
+        cursor: n-resize;
+      }
+    }
+
+    .left {
+      position: fixed;
+      height: calc(100vh - 10px);
+      width: 5px;
+      top: 5px;
+      left: 0;
+
+      &:hover {
+        cursor: w-resize;
+      }
+    }
+
+    .right {
+      position: fixed;
+      height: calc(100vh - 10px);
+      width: 5px;
+      top: 5px;
+      right: 0;
+
+      &:hover {
+        cursor: w-resize;
+      }
+    }
+    .top-left{
+      position: fixed;
+      height: 5px;
+      width: 5px;
+      top: 0;
+      left: 0;
+
+      &:hover {
+        cursor: nw-resize;
+      }
+    }
+    .top-right{
+      position: fixed;
+      height: 5px;
+      width: 5px;
+      top: 0;
+      right: 0;
+
+      &:hover {
+        cursor: sw-resize;
+      }
+    }
+    .bottom-right{
+      position: fixed;
+      height: 5px;
+      width: 5px;
+      bottom: 0;
+      right: 0;
+
+      &:hover {
+        cursor: nw-resize;
+      }
+    }
+    .bottom-left{
+      position: fixed;
+      height: 5px;
+      width: 5px;
+      bottom: 0;
+      left: 0;
+
+      &:hover {
+        cursor: sw-resize;
+      }
+    }
+  }
 }
 
 .titlebar-button {
@@ -88,11 +200,4 @@ const showTitlebar = import.meta.env?.VITE_TITLEBAR === 'true'
     color: #edeef2;
   }
 }
-
-.drag-area {
-  width: 100%;
-  height: 100%;
-  margin: 5px 0 0 5px;
-}
-
 </style>
