497d4cdaac2f22cd197ea41b9519803c15864153
Jian Guo <guojian_k@qq.com>
2024-05-13 02:15:55 +0800

    update

--- src/components/CanvasContextmenu.vue
+++ src/components/CanvasContextmenu.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
 import { addNode, createNodeTemplate, currentProject } from '@/stores/service/project-service'
 import { inject, type Ref } from 'vue'
+import { useSettings } from '@/stores/settings'
 
 const contextmenuRef = inject<Ref<HTMLElement>>('contextmenu')
 const canvasRef = inject<Ref<HTMLElement>>('canvas')
 
+const settings = useSettings()
 const project = currentProject()
+
+function getCanvasPointByClientPoint(x: number, y: number) {
+  const { x: cx, y: cy } = canvasRef.value.getBoundingClientRect()
+  return { x: x - cx - settings.offsetX - project.offset.x, y: y - cy - settings.offsetY - project.offset.y }
+}
 
 function handleNewNode() {
   const x = contextmenuRef.value.getAttribute('data-x')
   const y = contextmenuRef.value.getAttribute('data-y')
-  const box = canvasRef.value.getBoundingClientRect()
-  const cx = box.x
-  const cy = box.y
-  console.log(x, y, cx, cy, x - cx, y - cy,)
+  const { x: cx, y: cy } = getCanvasPointByClientPoint(x, y)
   const node = createNodeTemplate()
+  node.x = Math.round(cx / settings.unitWidth)
+  node.y = Math.round(cy / settings.unitHeight)
   addNode(project, node)
 }
 
