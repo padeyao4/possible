76c8a49353b6908280b1c5864de5498552ebfcbd
Jian Guo <guojian_k@qq.com>
2024-05-15 03:59:39 +0800

    优化结构

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,10 +1,8 @@
 import type { Point, Project } from '@/stores/projects'
-import { useSettings } from '@/stores/settings'
 
 export function getCanvasPointByOffsetPoint(point: Point, project: Project) {
-  const settings = useSettings()
   return {
-    x: point.x - settings.offsetX - project.offset.x,
-    y: point.y - settings.offsetY - project.offset.y
+    x: point.x - project.offset.x,
+    y: point.y - project.offset.y
   }
 }
