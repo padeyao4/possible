5553060f17eb1aa0193806b163615a42b000c47d
Jian Guo <guojian_k@qq.com>
2024-05-24 14:40:14 +0800

    feat: change background grid

--- src/components/canvas/CanvasGrid.vue
+++ src/components/canvas/CanvasGrid.vue
@@ -6,12 +6,20 @@ import { computed } from 'vue'
 const settings = useSettings()
 const project = currentProject()
 
-const translateX = computed(() => {
-  return project.offset.x % settings.unitWidth - settings.unitWidth + 'px'
+const translateX = computed(
+  () => (project.offset.x % settings.unitWidth) - settings.unitWidth + 'px'
+)
+
+const translateY = computed(() => {
+  return (project.offset.y % settings.unitHeight) - settings.unitHeight + 'px'
 })
 
-const size = computed(() => {
+const width = computed(() => {
   return settings.unitWidth + 'px'
+})
+
+const height = computed(() => {
+  return settings.unitHeight + 'px'
 })
 </script>
 
@@ -36,16 +44,26 @@ const size = computed(() => {
 }
 
 .container {
-  width: 150%;
-  height: 100%;
-  background-image: linear-gradient(
-    90deg,
-    transparent 0px,
-    transparent 119px,
-    rgba(27, 31, 35, 0.06) 119px,
-    rgba(27, 31, 35, 0.06) 120px
-  );
-  background-size: v-bind(size);
-  transform: translateX(v-bind(translateX));
+  width: 300%;
+  height: 300%;
+  background:
+    linear-gradient(
+        90deg,
+        transparent 0px,
+        transparent 119px,
+        rgba(27, 31, 35, 0.06) 119px,
+        rgba(27, 31, 35, 0.06) 120px
+      )
+      repeat,
+    linear-gradient(
+        0deg,
+        rgba(27, 31, 35, 0.06) 0px,
+        rgba(27, 31, 35, 0.06) 1px,
+        transparent 1px,
+        transparent 80px
+      )
+      repeat;
+  background-size: v-bind(width) v-bind(height);
+  transform: translateX(v-bind(translateX)) translateY(v-bind(translateY));
 }
 </style>
