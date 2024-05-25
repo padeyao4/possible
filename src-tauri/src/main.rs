dcb9158cc11c9bc8b69aed74148385d173787771
Jian Guo <guojian_k@qq.com>
2024-05-25 15:28:54 +0800

    feat: app单例

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{AppHandle, CustomMenuItem, Manager, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem, Window};
 use tauri::SystemTray;
+use tauri::{
+    AppHandle, CustomMenuItem, Manager, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem, Window,
+};
 use webbrowser;
 use window_shadows::set_shadow;
 
@@ -13,7 +15,9 @@ fn main() {
         .add_native_item(SystemTrayMenuItem::Separator)
         .add_item(CustomMenuItem::new("quit".to_string(), "退出"));
 
-    let tray = SystemTray::new().with_menu(tray_menu).with_tooltip("Possible for windows");
+    let tray = SystemTray::new()
+        .with_menu(tray_menu)
+        .with_tooltip("Possible for windows");
 
     tauri::Builder::default()
         .setup(|app| {
@@ -24,6 +28,12 @@ fn main() {
         .system_tray(tray)
         .on_system_tray_event(system_tray_handle)
         .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
+            //设置插件
+            let window = app.get_window("main").unwrap(); //二次打开软件时，显示已打开窗口，单例运行app
+            window.set_focus().unwrap();
+            window.show().unwrap();
+        }))
         .plugin(tauri_plugin_store::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
@@ -38,17 +48,15 @@ fn open_main_window(window: &Window) {
 fn system_tray_handle(app: &AppHandle, event: SystemTrayEvent) {
     let window = app.get_window("main").unwrap();
     match event {
-        SystemTrayEvent::MenuItemClick { id, .. } => {
-            match id.as_str() {
-                "quit" => app.exit(0),
-                "feedback" => {
-                    let url = "https://github.com/padeyao4/possible/issues";
-                    webbrowser::open(url).unwrap();
-                }
-                "open" => open_main_window(&window),
-                _ => {}
+        SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+            "quit" => app.exit(0),
+            "feedback" => {
+                let url = "https://github.com/padeyao4/possible/issues";
+                webbrowser::open(url).unwrap();
             }
-        }
+            "open" => open_main_window(&window),
+            _ => {}
+        },
         SystemTrayEvent::LeftClick { .. } => open_main_window(&window),
         SystemTrayEvent::RightClick { .. } => {}
         SystemTrayEvent::DoubleClick { .. } => {}
