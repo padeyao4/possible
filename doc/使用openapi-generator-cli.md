5f981579dca78ee28935eae9b8c92faf842b5e34
Jian Guo <guojian_k@qq.com>
2024-06-13 01:43:14 +0800

    feat: add openapi tools

---
+++ doc/使用openapi-generator-cli.md
@@ -0,0 +1,5 @@
+# 使用openapi生成axios代码
+
+```bash
+openapi-generator-cli generate -i http://10.8.0.1:8080/v3/api-docs -g typescript-axios -o ./src/openapi/
+```
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import App from './App.vue'
 import './assets/main.css'
 import router from './router'
 import { Icon } from '@iconify/vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_PATH ?? '/'
 
 const app = createApp(App)
 app.use(createPinia())
