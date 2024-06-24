f0474c76995a873e62984fb90786d2b1d1450c60
Jian Guo <guojian_k@qq.com>
2024-06-25 03:12:36 +0800

    refactor: app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import GithubCorner from '@/components/other/GithubCorner.vue';
 import emitter, { BusEvents, dataChangeEvents } from '@/utils/emitter';
-import { onBeforeMount, onBeforeUnmount } from 'vue';
+import { onBeforeUnmount } from 'vue';
 import { useAccount } from '@/stores/account';
 import { useProjectStore } from '@/stores/project';
 import { useDebounceFn } from '@vueuse/core';
@@ -9,51 +9,53 @@ import { useScheduler, useUpdateDate } from '@/service';
 import { useCounter } from '@/stores/counter';
 
 const account = useAccount();
-const projectStore = useProjectStore();
+const store = useProjectStore();
 const counter = useCounter();
 useScheduler();
 useUpdateDate();
 
-onBeforeMount(() => {
-  const debounceDataPushFnc = useDebounceFn(() => {
-    console.info('data push', new Date());
-    projectStore.push();
-  }, 1000);
+const debounceDataPushFnc = useDebounceFn(() => {
+  console.info('data push', new Date());
+  store.push();
+}, 1000);
 
-  emitter.on(BusEvents['login:success'], async () => {
-    console.info('login success');
-    await account.fetchUser();
-    console.info('start to fetch project');
-    emitter.emit(BusEvents['project:fetch']);
-  });
-  emitter.on(BusEvents['project:fetch'], async () => {
-    if (account.enable) {
-      await projectStore.fetch();
-    }
-    projectStore.dailyUpdate();
-    counter.countTodos();
-  });
-  emitter.on(BusEvents['time:updated'], () => {
-    console.info('daily update');
-    projectStore.dailyUpdate();
-    emitter.emit(BusEvents['project:updated']);
-  });
-  emitter.on('*', (event: any) => {
-    if (dataChangeEvents.has(event)) {
-      console.info('data change', event);
-      counter.countTodos();
-      if (account.enable) {
-        debounceDataPushFnc();
-      }
-      counter.countTodos();
-    }
-  });
-
-  // 账号如果是登录的
+emitter.on(BusEvents['login:success'], async () => {
+  console.info('login success');
+  await account.fetchUser();
+  console.info('start to fetch project');
+  emitter.emit(BusEvents['project:fetch']);
+});
+emitter.on(BusEvents['project:fetch'], async () => {
   if (account.enable) {
-    account.fetchUser().then(() => emitter.emit(BusEvents['project:fetch']));
+    console.info('start to fetch project');
+    await store.fetch();
+  }
+  console.info('start daily update');
+  store.dailyUpdate();
+  console.info('start count todos');
+  counter.countTodos();
+});
+emitter.on(BusEvents['time:updated'], () => {
+  console.info('daily update');
+  store.dailyUpdate();
+  emitter.emit(BusEvents['project:updated']);
+});
+emitter.on('*', (event: any) => {
+  if (dataChangeEvents.has(event)) {
+    console.info('data change', event);
+    counter.countTodos();
+    if (account.enable) {
+      debounceDataPushFnc();
+    }
+    counter.countTodos();
   }
 });
+
+// 账号如果是登录的
+if (account.enable) {
+  console.info('start to fetch user');
+  account.fetchUser().then(() => emitter.emit(BusEvents['project:fetch']));
+}
 
 onBeforeUnmount(() => {
   emitter.all.clear();
