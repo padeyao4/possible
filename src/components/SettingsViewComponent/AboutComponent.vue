1b271d0e5ffa726d3256d012ae022b97b8105dd9
Jian Guo <guojian_k@qq.com>
2024-07-02 04:01:16 +0800

    feat: 添加桌面图标

--- src/components/SettingsViewComponent/AboutComponent.vue
+++ src/components/SettingsViewComponent/AboutComponent.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import SettingsItem from './SettingsItem.vue'
+import SettingsItem from './SettingsItem.vue';
 </script>
 <template>
   <SettingsItem>
     <template #title>关于</template>
     <template #description>
-      <div class="version">当前版本: 0.1.12</div>
+      <div class="version">当前版本: 0.3.1</div>
       <div class="version">联系方式: guojian_k@qq.com</div>
     </template>
   </SettingsItem>
