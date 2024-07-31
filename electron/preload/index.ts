9e00d9cf92eed1e4ba303a450396fe338453f4bb
Jian Guo <guojian_k@qq.com>
2024-07-31 15:25:46 +0800

    feat: 通知electron取消加载页面

--- electron/preload/index.ts
+++ electron/preload/index.ts
@@ -2,19 +2,19 @@ import { ipcRenderer, contextBridge } from 'electron';
 
 // --------- Expose some API to the Renderer process ---------
 contextBridge.exposeInMainWorld('ipcRenderer', {
-  on(...args: Parameters) {
+  on(...args: Parameters<typeof ipcRenderer.on>) {
     const [channel, listener] = args;
     return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args));
   },
-  off(...args: Parameters) {
+  off(...args: Parameters<typeof ipcRenderer.off>) {
     const [channel, ...omit] = args;
     return ipcRenderer.off(channel, ...omit);
   },
-  send(...args: Parameters) {
+  send(...args: Parameters<typeof ipcRenderer.send>) {
     const [channel, ...omit] = args;
     return ipcRenderer.send(channel, ...omit);
   },
-  invoke(...args: Parameters) {
+  invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
     const [channel, ...omit] = args;
     return ipcRenderer.invoke(channel, ...omit);
   }
