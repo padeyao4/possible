982d2dd7b988b45fbd6c4df4f3fc0a0400bc4103
Jian Guo <guojian_k@qq.com>
2024-08-02 17:53:14 +0800

    feat: 调整颜色

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -23,7 +23,7 @@ const titleWidth = computed(() => {
 </script>
 
 <template>
-  <div class="flex h-screen flex-col border-l border-r bg-gray-50">
+  <div class="flex h-screen flex-col border-l border-r">
     <header
       class="drag-region flex h-16 shrink-0 items-end justify-start px-3 pb-3 text-xl text-gray-500"
     >
@@ -36,6 +36,7 @@ const titleWidth = computed(() => {
       style="
         grid-template-columns: 40px calc(100% - 40px);
         grid-template-rows: 40px calc(100vh - 153px) 48px;
+        background-color: #f2f4f7;
       "
     >
       <div class="flex h-full w-full items-center justify-center border-b border-r border-dashed">
@@ -44,7 +45,7 @@ const titleWidth = computed(() => {
       <canvas-header />
       <canvas-ruler />
       <the-canvas />
-      <project-footer class="col-span-2" />
+      <project-footer class="col-span-2" style="background-color: #fff" />
     </main>
   </div>
 </template>
