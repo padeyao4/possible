cb4d1a8d5127cd8a733d62d6b1ff7e5d13db33aa
Jian Guo <guojian_k@qq.com>
2025-02-13 23:32:18 +0800

    update

--- lib/component/nav.dart
+++ lib/component/nav.dart
@@ -4,92 +4,97 @@ import 'package:possible/model/node.dart';
 import 'package:possible/state/state.dart';
 import 'package:provider/provider.dart';
 
+List<Widget> getList(List<Node> projects, MyState state) {
+  return projects.map((project) {
+    return ListTile(
+      title: Text(project.name),
+      onTap: () {
+        state.setCurrent(project);
+        state.changePage(MyPage.project);
+      },
+    );
+  }).toList();
+}
+
 class NavigatorWidget extends StatelessWidget {
   const NavigatorWidget({super.key});
 
-  List<Widget> getList(List<Node> projects, MyState state) {
-    return projects.map((project) {
-      return GestureDetector(
-        onTap: () {
-          state.setCurrent(project);
-          state.changePage(MyPage.project);
-        },
-        child: ListTile(
-          title: Text(project.name),
-        ),
-      );
-    }).toList();
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 240,
+      child: Column(children: [
+        NavHeaderList(),
+        NavBodyList(),
+        NavBottom(),
+      ]),
+    );
   }
+}
+
+class NavBottom extends StatelessWidget {
+  const NavBottom({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var projects = context.watch<MyState>().projects;
-
-    return SizedBox(
-      width: 240,
-      child: Column(
-        children: [
-          GestureDetector(
-            onTap: () {
-              context.read<MyState>().changePage(MyPage.home);
-            },
-            child: const ListTile(
-              leading: Icon(Icons.home),
-              title: Text('我的一天'),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              context.read<MyState>().changePage(MyPage.backLog);
-            },
-            child: const ListTile(
-              leading: Icon(Icons.note),
-              title: Text('备忘录'),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              context.read<MyState>().changePage(MyPage.test);
-            },
-            child: const ListTile(
-              leading: Icon(Icons.check_circle),
-              title: Text('测试'),
-            ),
-          ),
-          const Divider(),
-          Expanded(
-              child: ListView(
-            children: getList(projects, context.read<MyState>()),
-          )),
-          const Divider(),
-          GestureDetector(
-            onTap: () {
-              context.read<MyState>().addProject(Node(
-                  id: projects.length.toString(),
-                  name: 'project-${projects.length}',
-                  index: DateTime.now().microsecond,
-                  position: Point(x: 0, y: 0)));
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Row(
-                  children: [
-                    Icon(Icons.add),
-                    Text('添加项目'),
-                  ],
-                ),
-                IconButton(
-                  icon: const Icon(Icons.settings),
-                  onPressed: () {
-                    // Add your settings action here
-                  },
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
+    return ListTile(
+      leading: const Icon(Icons.add),
+      title: const Text('新建项目'),
+      onTap: () {
+        // todo 弹出对话框,输入标题
+      },
     );
+  }
+}
+
+class NavBodyList extends StatelessWidget {
+  const NavBodyList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(context.watch<MyState>().projects[index].name),
+              );
+            },
+            itemCount: context.watch<MyState>().projects.length));
+  }
+}
+
+class NavHeaderList extends StatelessWidget {
+  const NavHeaderList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DrawerHeader(
+        padding: EdgeInsets.zero,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('我的一天'),
+              onTap: () {
+                context.read<MyState>().changePage(MyPage.home);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.note),
+              title: const Text('备忘录'),
+              onTap: () {
+                context.read<MyState>().changePage(MyPage.backLog);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.check_circle),
+              title: const Text('测试'),
+              onTap: () {
+                context.read<MyState>().changePage(MyPage.test);
+              },
+            ),
+          ],
+        ));
   }
 }
